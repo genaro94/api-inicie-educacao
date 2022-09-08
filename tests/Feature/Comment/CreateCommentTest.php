237e855e5aa8f43b53d7df1f9a1b5a515092b8ef
Genaro Rego Figueiredo <genarofigueiredo@hotmail.com>
2022-09-08 15:41:57 -0300

    created the test for the comment on a user's post

---
+++ tests/Feature/Comment/CreateCommentTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature\Comment;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+use Tests\TestCase;
+
+class CreateCommentTest extends TestCase
+{
+    private $token  = '7ba03f2c0a0909bd7c37efaadaa54777c2d0ff1d190878475c8fa953c3399609';
+    private $status = ['active', 'inactive'];
+    private $gender = ['female', 'male'];
+
+    public function test_create_comment_with_success(): void
+    {
+        $user = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+        ->post('/api/users', [
+            'name'   => fake()->name(),
+            'email'  => fake()->unique()->email(),
+            'gender' => Arr::random($this->gender),
+            'status' => Arr::random($this->status),
+        ]);
+
+        $post = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+        ->post('/api/posts', [
+           'user_id'  => $user['user']['id'],
+           'title'    => fake()->text(),
+           'body'     => fake()->text()
+        ]);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+        ->post('/api/comments', [
+           'post_id'  => $post['post']['id'],
+           'name'     => fake()->name(),
+           'email'    => fake()->unique()->email(),
+           'body'     => fake()->text()
+        ]);
+
+        $response->assertStatus(201)
+        ->assertJson([
+            'message' => 'Comment created successfully!',
+            'comment' => $response->original['comment']
+        ]);
+    }
+}
