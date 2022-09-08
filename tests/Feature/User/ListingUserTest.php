79d289beedb86406bccca7e3e00c83928560e9d5
Genaro Rego Figueiredo <genarofigueiredo@hotmail.com>
2022-09-07 21:55:03 -0300

    done the user listing test with filter by id.

--- tests/Feature/User/ListingUserTest.php
+++ tests/Feature/User/ListingUserTest.php
@@ -2,15 +2,11 @@
 
 namespace Tests\Feature\User;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Arr;
 use Tests\TestCase;
 
 class ListingUserTest extends TestCase
 {
     private $token  = '7ba03f2c0a0909bd7c37efaadaa54777c2d0ff1d190878475c8fa953c3399609';
-    private $status = ['active', 'inactive'];
-    private $gender = ['female', 'male'];
 
     public function test_user_listing_all(): void
     {
@@ -24,6 +20,14 @@ class ListingUserTest extends TestCase
     {
         $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
         ->get('/api/users?page=1');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_listing_with_filter_by_id(): void
+    {
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+        ->get('/api/users', ['id' => 2752]);
 
         $response->assertStatus(200);
     }
