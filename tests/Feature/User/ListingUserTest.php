9e3feb365428cb6cfd14f5bb93cc412ab4f73ece
Genaro Rego Figueiredo <genarofigueiredo@hotmail.com>
2022-09-07 21:07:12 -0300

    done the listing test of users with pagination

--- tests/Feature/User/ListingUserTest.php
+++ tests/Feature/User/ListingUserTest.php
@@ -2,16 +2,28 @@
 
 namespace Tests\Feature\User;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 use Tests\TestCase;
 
 class ListingUserTest extends TestCase
 {
     private $token  = '7ba03f2c0a0909bd7c37efaadaa54777c2d0ff1d190878475c8fa953c3399609';
+    private $status = ['active', 'inactive'];
+    private $gender = ['female', 'male'];
 
     public function test_user_listing_all(): void
     {
         $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
         ->get('/api/users');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_listing_with_pagination(): void
+    {
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+        ->get('/api/users?page=1');
 
         $response->assertStatus(200);
     }
