53c33e933bf5a289c0d31c1f16894baccc71a8e6
Genaro Rego Figueiredo <genarofigueiredo@hotmail.com>
2022-09-10 13:28:46 -0300

    done the test for creating a comment in the list of posts

---
+++ tests/Feature/Comment/CreateCommentListPostTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Comment;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+use Tests\TestCase;
+
+class CreateCommentListPostTest extends TestCase
+{
+    private $token         = '7ba03f2c0a0909bd7c37efaadaa54777c2d0ff1d190878475c8fa953c3399609';
+    private $status        = ['active', 'inactive'];
+    private $gender        = ['female', 'male'];
+    private $commentStatus = ['completed', 'pending'];
+
+    public function test_create_comment_in_post_list_with_success(): void
+    {
+        $user = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+        ->post('/api/users', [
+            'name'   => fake()->name(),
+            'email'  => fake()->unique()->email(),
+            'gender' => Arr::random($this->gender),
+            'status' => Arr::random($this->status),
+        ]);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+        ->post('/api/comments/store/list/posts', [
+           'user_id'  => $user['user']['id'],
+           'title'    => fake()->text(),
+           'due_on'   => \Carbon\Carbon::today(),
+           'status'   => Arr::random($this->commentStatus),
+        ]);
+
+        $response->assertStatus(201)
+        ->assertJson([
+            'message' => 'Comment created successfully!',
+            'comment' => $response->original['comment']
+        ]);
+    }
+}
