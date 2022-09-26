e5e23d100a448fe2827c4389eb782f06832ffbe1
Genaro Rego Figueiredo <genarofigueiredo@hotmail.com>
2022-09-26 12:00:12 -0300

    test destroy comment with success

---
+++ tests/Feature/Comment/DestroyCommentTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature\Comment;
+
+use Illuminate\Support\Arr;
+use Tests\TestCase;
+
+class DestroyCommentTest extends TestCase
+{
+    private $token  = '7ba03f2c0a0909bd7c37efaadaa54777c2d0ff1d190878475c8fa953c3399609';
+    private $status = ['active', 'inactive'];
+    private $gender = ['female', 'male'];
+
+    public function test_destroy_comment_with_success(): void
+    {
+        $user = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+        ->post('/api/users', [
+            'name'   => fake()->name(),
+            'email'  => fake()->unique()->email(),
+            'gender' => Arr::random($this->gender),
+            'status' => Arr::random($this->status),
+        ]);
+
+        $post = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+        ->post('/api/posts', [
+           'user_id'  => $user['data']['id'],
+           'title'    => fake()->text(),
+           'body'     => fake()->text()
+        ]);
+
+        $comment = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+        ->post('/api/comments', [
+           'post_id'  => $post['data']['id'],
+           'name'     => fake()->name(),
+           'email'    => fake()->email(),
+           'body'     => fake()->text()
+        ]);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+                         ->delete('/api/comments'.'/'. $comment['data']['id']);
+
+        $response->assertStatus(204);
+    }
+}
